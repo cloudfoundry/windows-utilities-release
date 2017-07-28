ca85b3f068b2b15485a47fbbdb79779a82687218
Matthew Horan <mhoran@pivotal.io>
2017-07-28 16:47:56 -0400

    initial commit - seems to work [#149444537]
    Signed-off-by: David Jahn <djahn@pivotal.io>

---
+++ jobs/set-admin-password/templates/pre-start.ps1
@@ -0,0 +1,12 @@
+﻿Start-Sleep 30
+
+<% if_p("set-admin-password.password") do |password| %>
+  Write-Host "Changing Administrator password for windows..."
+  $EncodedPass = "<%= Base64.strict_encode64(password) %>"
+  $NewPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedPass))
+  $AdminUser = [ADSI]"WinNT://${env:computername}/Administrator,User"
+  $AdminUser.SetPassword($NewPassword)
+
+  # Optionally prevent password from expiring
+  AdminUser.PasswordExpired = 0
+<% end %>
