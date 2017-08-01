b44ac72b27e12b9dbbb805353e38ec40fc1a9027
Lisa Oakley <loakley@pivotal.io>
2017-08-01 17:40:11 -0400

    Update enable-ssh job to modify local policies
    - To override LGPO on vsphere
    
    [#149440397](https://www.pivotaltracker.com/story/show/149440397)
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- jobs/enable-ssh/templates/pre-start.ps1
+++ jobs/enable-ssh/templates/pre-start.ps1
@@ -1,6 +1,20 @@
-﻿Start-Sleep 30
+﻿Start-Sleep 5
 
 $SSHDir="C:\Program Files\OpenSSH"
+$InfFilePath="C:\Windows\Temp\enable-ssh.inf"
+$InfFileContents=@'
+[Unicode]
+Unicode=yes
+[Version]
+signature=$CHICAGO$
+Revision=1
+[Registry Values]
+[System Access]
+[Privilege Rights]
+SeDenyNetworkLogonRight=*S-1-5-32-546
+SeAssignPrimaryTokenPrivilege=*S-1-5-19,*S-1-5-20,*S-1-5-80-3847866527-469524349-687026318-516638107-1125189541
+'@
+$LGPOPath="C:\Windows\LGPO.exe"
 
 if (-Not (Test-Path $SSHDir)) {
     Write-Error "OpenSSH does not appear to be installed: missing directory: $SSHDir"
@@ -17,6 +31,14 @@ if ((Get-NetFirewallRule | where { $_.DisplayName -eq 'SSH' }) -eq $null) {
     New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
 } else {
     "Firewall rule for SSH already exists"
+}
+
+if (Test-Path $LGPOPath) {
+    "Found $LGPOPath. Modifying security policies to support ssh."
+    Out-File -FilePath $InfFilePath -InputObject $InfFileContents -Force
+    & $LGPOPath /s $InfFilePath
+} else {
+    "Did not find $LGPOPath. Assuming existing security policies are sufficient to support ssh."
 }
 
 Push-Location $SSHDir
