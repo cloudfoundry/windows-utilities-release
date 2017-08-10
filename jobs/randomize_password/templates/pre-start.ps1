61fb7c93b07de9b34919d2a82000775cfdb531de
Charlie Vieth <cviethjr@pivotal.io>
2017-08-10 14:43:07 -0400

    Remove ps_modules package
    The ps_modules package was used to share 11 lines of code between
    `set_password` and `randomize_password`, but introduces a compilation
    step whenever these jobs are used - doubling deployment time.
    
    [#150146850](https://www.pivotaltracker.com/story/show/150146850)

--- jobs/randomize_password/templates/pre-start.ps1
+++ jobs/randomize_password/templates/pre-start.ps1
@@ -1,24 +1,86 @@
-﻿$Enabled=$<%= p("randomize_password.enabled").to_s %>
-if (-not $Enabled) { Exit 0 }
-
-. C:\var\vcap\packages\ps_modules\password.ps1
-
-Start-Sleep 5
-
-$ErrorActionPreference = "Stop";
+﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-[string]$NewPassword = Get-RandomPassword
+Start-Sleep 5 # Always sleep
 
-if ($NewPassword -eq '') {
-    throw "Error: password set to an empty string - refusing to change password"
+<% if p('randomize_password.enabled') %>
+
+function Is-Special() {
+    param([parameter(Mandatory=$true)] [string]$c)
+
+    return ('!' -le $c -and $c -le '/') -or (':' -le $c -and $c -le '@') -or `
+        ('[' -le $c -and $c -le '`') -or ('{' -le $c -and $c -le '~')
+}
+
+function Valid-Password() {
+    param([parameter(Mandatory=$true)] [string]$Password)
+
+    $digits = 0
+    $special = 0
+    $alphaLow = 0
+    $alphaHigh = 0
+
+    if ($Password.Length -lt 8) {
+        return $false
+    }
+    foreach ($c in $Password.ToCharArray()) {
+        if ('0' -le $c -and $c -le '9') {
+            $digits = 1
+        } elseif ('a' -le $c -and $c -le 'z') {
+            $alphaLow = 1
+        } elseif ('A' -le $c -and $c -le 'Z') {
+            $alphaHigh = 1
+        } elseif (Is-Special $c) {
+            $special = 1
+        } else {
+            # Invalid char
+            return $false
+        }
+    }
+    return ($digits + $special + $alphaLow + $alphaHigh) -ge 3
+}
+
+function Get-RandomPassword() {
+    $CharList = "!`"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_``abcdefghijklmnopqrstuvwxyz{|}~".ToCharArray()
+    $limit = 200
+    $count = 0
+
+    while ($limit-- -gt 0) {
+        $passwd = (Get-Random -InputObject $CharList -Count 24) -join ''
+        if (Valid-Password -Password $passwd) {
+            return $passwd
+        }
+    }
+    throw "Unable to generate a valid password after 200 attempts"
+    return $null
+}
+
+function Set-Password() {
+    param(
+        [parameter(Mandatory=$true)]
+        [string]$Username,
+        [parameter(Mandatory=$true)]
+        [string]$Password
+    )
+    $AdsiUser = [ADSI]"WinNT://${env:computername}/${Username},User"
+    $AdsiUser.SetPassword($Password)
+    $AdsiUser.PasswordExpired = 0
+    $AdsiUser.setinfo()
 }
 
 [string]$Username = '<%= p("randomize_password.username") %>'
-
 if ($Username -eq '') {
     throw "Error: empty user name - refusing to change password"
 }
 
+[string]$NewPassword = Get-RandomPassword
+if ($NewPassword -eq '') {
+    throw "Error: generating random password"
+}
+
 "Setting password for user: $Username"
 Set-Password -Username $Username -Password $NewPassword
+
+<% end %>
+
+Exit 0
