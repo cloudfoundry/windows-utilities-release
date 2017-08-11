6f32a2b6a6c0cd849c3063a6368fe6b4882263ef
Charlie Vieth <cviethjr@pivotal.io>
2017-08-11 11:10:42 -0400

    Remove ps_modules package
    The ps_modules package was used to share 11 lines of code between
    `set_password` and `randomize_password`, but introduces a compilation
    step whenever these jobs are used - doubling deployment time.
    
    [#150146850](https://www.pivotaltracker.com/story/show/150146850)
    
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- jobs/set_password/templates/pre-start.ps1
+++ jobs/set_password/templates/pre-start.ps1
@@ -1,30 +1,33 @@
-﻿$Enabled=$<%= p("set_password.enabled").to_s %>
-if (-not $Enabled) { Exit 0 }
-
-. C:\var\vcap\packages\ps_modules\password.ps1
-Start-Sleep 5
-
-$ErrorActionPreference = "Stop";
+﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-<%
-    if p("set_password.password").to_s.empty?
-        throw "password must be specified"
-    end
-%>
+Start-Sleep 5 # Always sleep
 
-[string]$EncodedPass = '<%= Base64.strict_encode64(p("set_password.password")) %>'
-[string]$NewPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedPass))
+<% if p('set_password.enabled') %>
 
-if ($NewPassword -eq '') {
-    throw "Error: password set to an empty string - refusing to change password"
+function Set-Password() {
+    param(
+        [parameter(Mandatory=$true)]
+        [string]$Username,
+        [parameter(Mandatory=$true)]
+        [string]$Password
+    )
+    $AdsiUser = [ADSI]"WinNT://${env:computername}/${Username},User"
+    $AdsiUser.SetPassword($Password)
+    $AdsiUser.PasswordExpired = 0
+    $AdsiUser.setinfo()
 }
 
 [string]$Username = '<%= p("set_password.username") %>'
-
+[string]$EncodedPass = '<%= Base64.strict_encode64(p("set_password.password")) %>'
+[string]$NewPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedPass))
 if ($Username -eq '') {
     throw "Error: empty user name - refusing to change password"
 }
 
 "Setting password for user: $Username"
 Set-Password -Username $Username -Password $NewPassword
+
+<% end %>
+
+Exit 0
