f27204ee975f61c3872fabcdc281c77bc4ca7378
Lisa Oakley <loakley@pivotal.io>
2017-12-11 10:37:01 -0500

    Ensure Administrator user is active and part of the Administrators group when calling set_password
    - testing for user's existence is unnecessary because net add will be a no-op if it already exists.
    
    [#151530999]https://www.pivotaltracker.com/story/show/151530999
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- jobs/set_password/templates/pre-start.ps1
+++ jobs/set_password/templates/pre-start.ps1
@@ -5,29 +5,6 @@ Start-Sleep 5 # Always sleep
 
 <% if p('set_password.enabled') %>
 
-function Set-Password() {
-    param(
-        [parameter(Mandatory=$true)]
-        [string]$Username,
-        [parameter(Mandatory=$true)]
-        [string]$Password
-    )
-    $AdsiUser = [ADSI]"WinNT://${env:computername}/${Username},User"
-    $AdsiUser.SetPassword($Password)
-    $AdsiUser.PasswordExpired = 0
-    $AdsiUser.setinfo()
-}
-
-function Test-User() {
-    param(
-        [parameter(Mandatory=$true)]
-        [string]$Username
-    )
-
-    $User = Get-WMiObject -class Win32_UserAccount | Where {$_.Name -eq $Username}
-    return $User -ne $null
-}
-
 [string]$Username = '<%= p("set_password.username") %>'
 [string]$EncodedPass = '<%= Base64.strict_encode64(p("set_password.password")) %>'
 [string]$NewPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedPass))
@@ -35,14 +12,17 @@ if ($Username -eq '') {
     throw "Error: empty user name - refusing to change password"
 }
 
-if (($Username -eq 'Administrator') -and !(Test-User -Username $Username)) {
-    "Adding new user: $Username"
+if ($Username -eq 'Administrator')  {
+    "Ensure user [$Username] is created"
     net user $Username $NewPassword /add
+    "Ensure user is active"
+    net user $Username /active:yes
+    "Ensure user is an Admin"
     net localgroup Administrators $Username /add
-} else {
-    "Setting password for user: $Username"
-    Set-Password -Username $Username -Password $NewPassword
 }
+
+"Setting password for user: $Username"
+net user $Username $NewPassword
 
 <% end %>
 
