8eecd4ef84e2b895678875bbf117efa461b1dd3d
Simon Jones <sijones@pivotal.io>
2018-04-27 14:22:47 -0400

    Redirect all streams from Disable-Rdp to out
    [#156857539](https://www.pivotaltracker.com/story/show/156857539)
    
    Signed-off-by: Zander Mackie <amackie@pivotal.io>

--- jobs/enable_rdp/templates/drain.ps1
+++ jobs/enable_rdp/templates/drain.ps1
@@ -21,7 +21,7 @@ trap {
 $dir = Split-Path $MyInvocation.MyCommand.Path
 . "$dir\disable-rdp.ps1"
 
-Disable-RDP | Out-File -FilePath $OutLog -Encoding ascii
+Disable-RDP *>&1 | Out-File -FilePath $OutLog -Encoding ascii
 
 "0"
 Exit 0
