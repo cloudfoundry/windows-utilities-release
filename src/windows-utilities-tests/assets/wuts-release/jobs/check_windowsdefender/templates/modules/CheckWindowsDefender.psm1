20d112cdb6bcaf8581611bac1cf9ab3aa248162f
aram price <aram.price@broadcom.com>
2025-07-14 14:45:45 -0700

    wuts: formatting and logging consistency

--- src/windows-utilities-tests/assets/wuts-release/jobs/check_windowsdefender/templates/modules/CheckWindowsDefender.psm1
+++ src/windows-utilities-tests/assets/wuts-release/jobs/check_windowsdefender/templates/modules/CheckWindowsDefender.psm1
@@ -24,24 +24,24 @@ function Assert-DefenderEnabled {
     $enabled = $True
 
     Get-MpPreference |
-        Select-Object -ExpandProperty CimInstanceProperties |
-        Where-Object { $_.Name -Like "Disable*"} |
-        ForEach-Object {
-            $propertyName = $_.Name.Replace('Disable', '')
+            Select-Object -ExpandProperty CimInstanceProperties |
+            Where-Object { $_.Name -Like "Disable*" } |
+            ForEach-Object {
+                $propertyName = $_.Name.Replace('Disable', '')
 
-            if ( $ExpectedEnabledProperties -Contains $propertyName ) {
-                if ( $_.Value -eq $True ) {
-                    Write-Log "Expected $($propertyName) to be enabled, it is disabled"
-                    $enabled = $False
+                if ($ExpectedEnabledProperties -Contains $propertyName) {
+                    if ($_.Value -eq $True) {
+                        Write-Log "Expected $( $propertyName ) to be enabled, it is disabled"
+                        $enabled = $False
+                    }
+                }
+                if ($ExpectedDisabledProperties -Contains $propertyName) {
+                    if ($_.Value -eq $False) {
+                        Write-Log "Expected $( $propertyName ) to be disabled, it is enabled"
+                        $enabled = $False
+                    }
                 }
             }
-            if ( $ExpectedDisabledProperties -Contains $propertyName ) {
-                if ( $_.Value -eq $False ) {
-                    Write-Log "Expected $($propertyName) to be disabled, it is enabled"
-                    $enabled = $False
-                }
-            }
-        }
 
     return $enabled
 }
@@ -49,17 +49,17 @@ function Assert-DefenderEnabled {
 function Assert-DefenderDisabled {
     Write-Log "Expected DisableIntrusionPreventionSystem to be disabled, it is enabled"
     $disabled = $True
-    $AllProperties =  $ExpectedEnabledProperties + $ExpectedDisabledProperties
+    $AllProperties = $ExpectedEnabledProperties + $ExpectedDisabledProperties
 
     Get-MpPreference |
-    Select-Object -ExpandProperty CimInstanceProperties |
-    Where-Object { $_.Name -Like "Disable*"} |
-    ForEach-Object {
+            Select-Object -ExpandProperty CimInstanceProperties |
+            Where-Object { $_.Name -Like "Disable*" } |
+            ForEach-Object {
                 $propertyName = $_.Name.Replace('Disable', '')
 
-                if ( $AllProperties -Contains $propertyName ) {
-                    if ( $_.Value -eq $False ) { #if value is false than property is enaled
-                        Write-Log "Expected $propertyName to be disabled, it is enabled"
+                if ($AllProperties -Contains $propertyName) {
+                    if ($_.Value -eq $False) { # if value is false then the property is enaled
+                        Write-Log "Expected $( $propertyName ) to be disabled, it is enabled"
                         $disabled = $False
                     }
                 }
